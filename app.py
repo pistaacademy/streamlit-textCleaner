0fe50a151105e5fe20214453b677a0ded8537cdc
pistaacademy <pista.academy@gmail.com>
2023-12-19 07:04:13 +0330

    Session-2

--- app.py
+++ app.py
@@ -2,6 +2,28 @@ import streamlit as st
 import neattext.functions as nfx
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
+import base64
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+from wordcloud import WordCloud
+
+def plot_wordcloud(my_text):
+    my_wordcloud = WordCloud().generate(my_text)
+    fig = plt.figure()
+    plt.imshow(my_wordcloud, interpolation="bilinear")
+    plt.axis('Off')
+    st.pyplot(fig)
+
+
+def text_downloader(raw_text):
+    b64 = base64.b64encode(raw_text.encode()).decode()
+    new_filename = "clean_text_result_{}_.txt".format(timestr)
+    st.markdown("### ** 📩 ⬇️ Download Cleaned Text file **")
+    href = f'<a href="data:file/txt;base64,{b64}" download="{new_filename}">Click here!</a>'
+    st.markdown(href, unsafe_allow_html=True) 
 
 def main():
     st.title("Text Cleaner App")
@@ -18,6 +40,7 @@ def main():
         clean_punctuations = st.sidebar.checkbox("Punctuations")
         clean_email = st.sidebar.checkbox("Emails")
         clean_urls = st.sidebar.checkbox("Urlds")
+        clean_numbers = st.sidebar.checkbox("Numbers")
 
 
         if text_file is not None:
@@ -38,7 +61,12 @@ def main():
                             raw_text = nfx.remove_emails(raw_text)
                         if clean_urls:
                             raw_text = nfx.remove_urls(raw_text)
+                        if clean_numbers:
+                            raw_text = nfx.remove_numbers(raw_text)
                         st.write(raw_text)
+                        text_downloader(raw_text)
+            with st.expander("Plot Wordcloud"):
+                plot_wordcloud(raw_text)
     else:
         st.subheader("Text Cleaner App")
 
